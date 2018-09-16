d18c17986eda3c34e2a72b8d70230a367fb52684
jterrill <joshterrill.dev@gmail.com>
2018-09-15 20:53:24 -0700

    added app json and start script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const version = require('./package.json').version;
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`API Version: ${version}.<br /><a href="https://github.com/joshterrill/hangouts-call-api-server#readme">View README</a>`);
